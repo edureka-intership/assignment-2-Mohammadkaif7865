b5074fbb25dae3246dc83a582069304f6e77cfc7
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-04-25 14:04:24 +0530

    yes me

--- myScript.js
+++ myScript.js
@@ -1,14 +1,14 @@
-let cart = 0;
+let cart = new Array(50).fill(0);
 
-function Add_check() {
-    let a = document.getElementById('check-not-check');
-    if (cart % 2 == 0) {
+function Add_check(x) {
+    let a = document.getElementById(`check-not-check-${x}`);
+    if (cart[x] % 2 == 0) {
         a.classList.remove('bi-plus-lg');
         a.classList.add('bi-check2');
-        cart++;
+        cart[x]++;
     } else {
         a.classList.remove('bi-check2');
         a.classList.add('bi-plus-lg');
-        cart++;
+        cart[x]--;
     }
 }
